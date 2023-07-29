65c9fb09e01b97044b4af9846c7a27cf0d85d813
jakubkrhovjak <c_jkrhovjak@groupon.com>
2023-07-29 11:27:49 +0200

    java code

---
+++ src/test/java/com/topleader/topleader/TopLeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topleader.topleader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopLeaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
