1be8a738b8f5a43150ad72c3828d9624d366f056
jakubkrhovjak <c_jkrhovjak@groupon.com>
2023-08-05 15:45:26 +0200

    added prod db properties

--- src/main/java/com/topleader/topleader/TopLeaderApplication.java
+++ src/main/java/com/topleader/topleader/TopLeaderApplication.java
@@ -2,12 +2,26 @@ package com.topleader.topleader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RestController
 @SpringBootApplication
 public class TopLeaderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TopLeaderApplication.class, args);
+    }
+
+
+    @GetMapping("/api/public/test")
+    public String test() {
+        return "App ok";
     }
 
 }
--- src/main/java/com/topleader/topleader/configuration/WebSecurityConfig.java
+++ src/main/java/com/topleader/topleader/configuration/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/api/public/**").permitAll()
+                .requestMatchers("/_ah/start", "/api/public/**").permitAll()
                 .anyRequest().authenticated()
             )
             .httpBasic(withDefaults())
