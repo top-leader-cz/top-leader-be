b6ec508466d68c34500540efddc2836172fb6ad1
jakubkrhovjak <c_jkrhovjak@groupon.com>
2024-04-11 07:44:57 +0200

    prompt fix

--- src/main/java/com/topleader/topleader/ai/AiClient.java
+++ src/main/java/com/topleader/topleader/ai/AiClient.java
@@ -40,6 +40,7 @@ public class AiClient {
     public String findActionGoal(String locale, List<String> strengths, List<String> values, List<String> development, String longTermGoal, List<String> actionsSteps) {
         log.info("Finding personal growth for strengths: {} and values: {} locale: {}", strengths, values, locale);
         var prompt = aiPromptService.getPrompt(AiPrompt.PromptType.PERSONAL_GROWTH_TIP);
+        log.info("prompt: {}", prompt);
         return chatClient.call(MessageFormat.format(prompt, strengths, values, development, longTermGoal, actionsSteps, locale));
     }
 
