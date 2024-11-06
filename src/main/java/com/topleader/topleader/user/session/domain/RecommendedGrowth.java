53450723ee406d60b6016fe5270b45718c5dd18c
Jakub Krhovják <jakub.krhovjak@protonmail.com>
2024-11-06 16:32:34 +0100

    fix PERSONAL_GROWTH_TIP (#70)
    Co-authored-by: jakubkrhovjak <c_jkrhovjak@groupon.com>

---
+++ src/main/java/com/topleader/topleader/user/session/domain/RecommendedGrowth.java
@@ -0,0 +1,11 @@
+package com.topleader.topleader.user.session.domain;
+
+import lombok.Data;
+
+@Data
+public class RecommendedGrowth {
+
+    private String area;
+    private String recommendation;
+
+}
