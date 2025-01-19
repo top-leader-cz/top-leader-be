86e373a29609104334db06224d598dda631f7ff5
Daniel Slavík <daniel.slavik@pricefx.eu>
2025-01-19 21:28:28 +0100

    fix(internal-rate): incorrect internal rate setup fixed

--- src/main/resources/db/migration/0.0.1/V0.0.1.60__internal_rate.sql
+++ src/main/resources/db/migration/0.0.1/V0.0.1.60__internal_rate.sql
@@ -1,4 +1,3 @@
 alter table coach add column internal_rate integer default 1;
 
 update coach set internal_rate = cr.rate_credit from coach c left join coach_rate cr on c.rate = cr.rate_name;
-update coach set internal_rate = 165 where internal_rate = 110;
