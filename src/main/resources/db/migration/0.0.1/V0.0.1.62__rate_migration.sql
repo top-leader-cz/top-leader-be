c7a960e4bb186d6d2b5194bcf7bb5a4f31b4871c
Daniel Slavík <daniel.slavik@pricefx.eu>
2025-01-24 20:21:54 +0100

    fix(coach-rate): internal rates fixed based on the current coach rate value + $$ coach rate changed.

---
+++ src/main/resources/db/migration/0.0.1/V0.0.1.62__rate_migration.sql
@@ -0,0 +1,5 @@
+update coach set internal_rate = 110 where rate = '$';
+update coach set internal_rate = 165 where rate = '$$';
+update coach set internal_rate = 275 where rate = '$$$';
+
+update coach_rate set rate_credit = 165 where rate_name = '$';
