0ecc2c4ffce3157d83c13b7c980c14704f360bae
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-01-29 17:20:51 +0100

    TOP-266 Calendly rewrite url fix

--- src/main/java/com/topleader/topleader/calendar/calendly/CalendlyController.java
+++ src/main/java/com/topleader/topleader/calendar/calendly/CalendlyController.java
@@ -2,6 +2,7 @@ package com.topleader.topleader.calendar.calendly;
 
 
 import com.topleader.topleader.calendar.domain.CalendarSyncInfo;
+import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,24 +23,27 @@ public class CalendlyController {
 
     @GetMapping("/login/calendly")
     public RedirectView cal(String code) {
-        var tokens = calendlyService.fetchTokens(code);
-        var userInfo = calendlyService.getUserInfo(tokens);
-
-        var info = new CalendarSyncInfo()
-                .setId(new CalendarSyncInfo.CalendarInfoId(userInfo.getResource().getEmail(), CalendarSyncInfo.SyncType.CALENDLY))
-                .setRefreshToken(tokens.getRefreshToken())
-                .setAccessToken(tokens.getAccessToken())
-                .setOwnerUrl(tokens.getOwner())
-                .setStatus(CalendarSyncInfo.Status.OK)
-                .setLastSync(LocalDateTime.now());
+        CalendarSyncInfo info;
+        try {
+            var tokens = calendlyService.fetchTokens(code);
+            var userInfo = calendlyService.getUserInfo(tokens);
+             info = new CalendarSyncInfo()
+                    .setId(new CalendarSyncInfo.CalendarInfoId(userInfo.getResource().getEmail(), CalendarSyncInfo.SyncType.CALENDLY))
+                    .setRefreshToken(tokens.getRefreshToken())
+                    .setAccessToken(tokens.getAccessToken())
+                    .setOwnerUrl(tokens.getOwner())
+                    .setStatus(CalendarSyncInfo.Status.OK)
+                    .setLastSync(LocalDateTime.now());
+        } catch (Exception e) {
+            log.error("Failed to sync Calendly", e);
+            return new RedirectView("/#/sync-failure?provider=calendly&error=sync.failed");
+        }
 
         log.info("Saving Calendly info: {}", info.getId().getUsername());
 
-        calendlyService.saveInfo(info);
-
-        return new RedirectView("/#/sync-success?provider=calendly");
-
+        return Try.run(() -> calendlyService.saveInfo(info))
+                .map(d -> new RedirectView("/#/sync-success?provider=calendly"))
+                .onFailure(e -> log.error("Failed to save Calendly info", e))
+                .getOrElse(new RedirectView("/#/sync-failure?provider=calendly&error=user.email.not.matched"));
     }
-
-
 }
