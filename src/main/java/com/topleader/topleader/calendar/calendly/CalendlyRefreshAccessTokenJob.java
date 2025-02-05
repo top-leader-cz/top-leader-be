8db31785a077db10a8b9b08cdcabb7168f3963d6
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-02-05 18:45:35 +0100

    updated availability tests

--- src/main/java/com/topleader/topleader/calendar/calendly/CalendlyRefreshAccessTokenJob.java
+++ src/main/java/com/topleader/topleader/calendar/calendly/CalendlyRefreshAccessTokenJob.java
@@ -5,6 +5,7 @@ import com.topleader.topleader.calendar.CalendarSyncInfoRepository;
 import com.topleader.topleader.calendar.CalendarSyncInfoService;
 import com.topleader.topleader.calendar.CalendarToErrorHandler;
 import com.topleader.topleader.calendar.domain.CalendarSyncInfo;
+import com.topleader.topleader.util.common.CommonUtils;
 import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -34,6 +36,7 @@ public class CalendlyRefreshAccessTokenJob {
         log.info("Fetching Calendly tokens");
         repository.findBySyncType(CalendarSyncInfo.SyncType.CALENDLY)
                 .forEach(info -> {
+                    CommonUtils.sleep(TimeUnit.MILLISECONDS, 100);
                     log.info("fetching tokens for user: {}", info.getUsername());
                     Try.of(() -> calendarSyncInfoService.fetchTokens(info))
                             .map(tokens ->
