d334b35bb42602653471eec469a4b8bac234d2ce
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-03-02 19:36:35 +0100

    TOP-284 24 availability

--- src/main/java/com/topleader/topleader/calendar/CalendarSyncInfoRepository.java
+++ src/main/java/com/topleader/topleader/calendar/CalendarSyncInfoRepository.java
@@ -1,14 +1,12 @@
 package com.topleader.topleader.calendar;
 
 import com.topleader.topleader.calendar.domain.CalendarSyncInfo;
-import com.topleader.topleader.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CalendarSyncInfoRepository extends JpaRepository<CalendarSyncInfo, CalendarSyncInfo.CalendarInfoId> {
 
     List<CalendarSyncInfo> findBySyncType(CalendarSyncInfo.SyncType syncType);
+
 }
