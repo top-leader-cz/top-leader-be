2be07f2224dd505094e815c34042e9ec149814a4
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-03-05 12:34:21 +0100

    TOP-284 24 availability

--- src/main/resources/db/migration/0.0.1/V0.0.1.66__calendar_email.sql
+++
@@ -1,5 +0,0 @@
-alter table calendar_sync_info add column email varchar(500) null;
-create index calendar_sync_info_email_idx on  calendar_sync_info(email);
-
--- drop table if exists calendly_info;
-drop table if exists google_calendar_sync_info;
