1443e7c4b9eef70e4252113d72599389c7f30a61
cjkrhovjak-source <c_jkrhovjak@groupon.com>
2026-01-05 16:09:55 +0100

    repakage

---
+++ src/test/java/com/topleader/topleader/common/calendar/calendly/CalendlyControllerIT.java
@@ -0,0 +1,65 @@
+package com.topleader.topleader.common.calendar.calendly;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.topleader.topleader.IntegrationTest;
+import com.topleader.topleader.TestUtils;
+import com.topleader.topleader.common.calendar.CalendarSyncInfoRepository;
+import com.topleader.topleader.common.calendar.domain.CalendarSyncInfo;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql("/sql/calendly/calendly-token.sql")
+@Disabled
+class CalendlyControllerIT extends IntegrationTest {
+
+    @Autowired
+    CalendarSyncInfoRepository repository;
+
+
+    @Test
+    @WithMockUser(authorities = "JOB")
+    void calendlyLogin() throws Exception {
+        mockServer.stubFor(WireMock.post(urlEqualTo("/oauth/token"))
+                .withHeader(AUTHORIZATION, equalTo("Basic Ti1LWEROQTQ3Q19hRnYtdWxIZjRCRnNyaDd0T0F6RFNBY1J0S3VNRERYSToyUVJEVGkyME5XV0FCTlpMczQxYmk4cFBGMVI3NEJCTnFPbUxDUzRDRnJz"))
+//                .withRequestBody(equalTo("grant_type%3Dauthorization_code%26code%3Dcode%26redirect_uri%3Dhttp%3A%2F%2Flocalhost%3A8080%2Flogin%2Fcalendly%3Fusername%3Dcoach1"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(TestUtils.readFileAsString("json/coach/calendly-token-response.json"))));
+
+
+        mockServer.stubFor(WireMock.get(urlEqualTo("/users/ownerId"))
+                .withHeader(AUTHORIZATION, equalTo("Bearer accessToken"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("""
+                                {
+                                  "resource": {
+                                    "email": "coach1"
+                                  }
+                                }
+                                """)));
+
+        mvc.perform(MockMvcRequestBuilders.get("/login/calendly?code=code&username=coach1"))
+                .andExpect(status().is3xxRedirection());
+
+        Assertions.assertThat(repository.findAll()).extracting(CalendarSyncInfo::getAccessToken, CalendarSyncInfo::getRefreshToken,
+                        c -> c.getId().getUsername(), CalendarSyncInfo::getOwnerUrl, CalendarSyncInfo::getSyncType, CalendarSyncInfo::getStatus)
+                .containsExactly(Assertions.tuple("accessToken", "refreshToken", "coach1",
+                        "http://localhost:8080/ownerId", CalendarSyncInfo.SyncType.CALENDLY, CalendarSyncInfo.Status.OK));
+    }
+
+}
