ded5a8cda3c5f3c59ffa46c3a292d54ccfa3faad
cjkrhovjak-source <c_jkrhovjak@groupon.com>
2026-03-01 08:37:53 +0100

    companies fetch fix

--- src/main/resources/db/migration/R__views.sql
+++ src/main/resources/db/migration/R__views.sql
@@ -199,8 +199,8 @@ select distinct ROW_NUMBER() OVER (ORDER BY u.username, s.time) AS id,
         s.time as date
 from users u
          left join scheduled_session s on s.username = u.username
-where u.status != 'CANCELED' and company_id is not null
-
+where u.status != 'CANCELED' and company_id is not null;
+CREATE INDEX IF NOT EXISTS users_coach_idx ON users(coach);
 
 
 
