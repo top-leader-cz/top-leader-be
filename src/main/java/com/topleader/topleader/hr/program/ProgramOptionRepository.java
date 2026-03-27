7cafaf1014e3e58730f33cd9e8dcb71c493a8702
redo <jakub.krhovjak@protonmail.com>
2026-03-27 05:30:43 +0100

    programs participants

--- src/main/java/com/topleader/topleader/hr/program/ProgramOptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.topleader.topleader.hr.program;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface ProgramOptionRepository extends ListCrudRepository<ProgramOption, String> {
-}
